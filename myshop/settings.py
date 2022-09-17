07e3e8b728eef5e5a9137a13f985ead96dc15bb5
NurgazyKG <nbaigonchokov@gmail.com>
2022-09-17 17:56:53 +0600

    req_add

--- myshop/settings.py
+++ myshop/settings.py
@@ -100,9 +100,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = '/home/PycharmProject/myshop/static/shop'
+STATIC_ROOT = '/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
